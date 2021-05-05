a18149b338e97fe9a8a3936f2de16f4e95ed012b
VKint <kint@xp.network>
2021-05-05 19:15:27 +0530

    fix transaction signing

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,7 @@ const main = async () => {
 
     async function send(transaction: any, to?: string) {
         let options = {
+            from: account.address,
             to  : to,
             data: transaction.encodeABI(),
             gas : gas.gasLimit,
@@ -39,24 +40,22 @@ const main = async () => {
     let deployed = await send(contract.deploy({data: byteCode}));
     console.log("Deployed contract address:", deployed.contractAddress);
 
-    const rawTransaction = {
+    const tx = await account.signTransaction({
         "from": account.address,
-        "nonce": web3.utils.toHex(await web3.eth.getTransactionCount(account.address)),
-        "gasPrice": web3.utils.toHex(web3.utils.toWei('70', 'gwei')),
+        "nonce": await web3.eth.getTransactionCount(account.address),
+        "gasPrice": gasP,
         "gas": web3.utils.toHex(gas.gasLimit),
         "to": deployed.contractAddress,
         "value": web3.utils.toHex(web3.utils.toWei('0.00005', 'ether')),
-        "chainId": 3
-    };
+        "chainId": 3,
+    });
 
-    const tx = new Transaction(rawTransaction);
-    tx.sign(Buffer.from(args.key, 'hex'));
-    const txid = await web3.eth.sendSignedTransaction(`0x${tx.serialize().toString('hex')}`);
-    console.log("sent wei to contract. hash: ", txid)
+    const txid = await web3.eth.sendSignedTransaction(tx.rawTransaction!!);
+    console.log("sent wei to contract. hash:", txid.transactionHash)
     contract = new web3.eth.Contract(abi, deployed.contractAddress);
 
-    contract.methods.send().send({from: account.address}).on('transactionHash', (hash: string) => {
-        console.log('Called contract send. Hash: ', hash);
-    })
+    let sendTx = await send(contract.methods.send(), deployed.contractAddress);
+    console.log("Executed contract! TxHash:", sendTx.transactionHash)
 }
+
 main()
